348a14c850c56da46e553e500124f61759d13137
Bernardo de Oliveira Bruning <bernardo.bruning@resultadosdigitais.com.br>
2020-01-03 23:09:10 -0300

    extract vector file

--- main.go
+++ main.go
@@ -9,47 +9,6 @@ import (
 	ui "github.com/gizak/termui/v3"
 )
 
-type Vector struct {
-	X, Y float64
-}
-
-func (source Vector) Sub(target Vector) Vector {
-	return NewVector(source.X-target.X, source.Y-target.Y)
-}
-
-func NewVector(x float64, y float64) Vector {
-	return Vector{X: x, Y: y}
-}
-
-func (target Vector) Len() float64 {
-	return math.Sqrt(math.Abs(target.X*target.X + target.Y*target.Y))
-}
-
-func (target Vector) Normalize() Vector {
-	len := target.Len()
-	return NewVector(target.X/len, target.Y/len)
-}
-
-func (target Vector) Invert() Vector {
-	return NewVector(target.Y, target.X)
-}
-
-func (target Vector) Min(source Vector) Vector {
-	return NewVector(math.Min(source.X, target.X), math.Min(source.Y, target.Y))
-}
-
-func (target Vector) Max(source Vector) Vector {
-	return NewVector(math.Max(source.X, target.X), math.Max(source.Y, target.Y))
-}
-
-func (target *Vector) Diff(source Vector) Vector {
-	return target.Max(source).Sub(target.Min(source))
-}
-
-func (v *Vector) ToPointer() image.Point {
-	return image.Pt(int(v.X), int(v.Y))
-}
-
 type Triangle struct {
 	a Vector
 	b Vector
