16f2b22403588422e30292951bf1dcfe4d1b7dd2
Bernardo de Oliveira Bruning <bernardo.bruning@resultadosdigitais.com.br>
2020-01-19 20:47:48 -0300

    deacouple pixel from canvas

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"math/rand"
 
 	"github.com/bernardo-bruning/termrender/render"
 	"github.com/bernardo-bruning/termrender/render/pixel"
+	p "github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
 
 func run() {
-	canvas := pixel.NewCanvas()
-	for !canvas.Win.Closed() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Term render",
+		Bounds: p.R(0, 0, 800, 800),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, int(win.Bounds().W()), int(win.Bounds().H())))
+	for !win.Closed() {
 		for i := 0; i < 100; i++ {
 			triangle := render.NewRandTriangle(0, 800)
 			r := uint8(rand.Intn(255))
 			g := uint8(rand.Intn(255))
 			b := uint8(rand.Intn(255))
-			triangle.Draw(canvas, color.RGBA{R: r, G: g, B: b})
+			triangle.Draw(img, color.RGBA{R: r, G: g, B: b})
 		}
-		pixel.Render(canvas)
+		pixel.Render(win, img)
+		img = image.NewRGBA(image.Rect(0, 0, int(win.Bounds().W()), int(win.Bounds().H())))
 	}
 }
 
--- render/pixel/main.go
+++ render/pixel/main.go
@@ -20,28 +20,10 @@ func (c *CanvasPixelGl) Set(x, y int, color color.Color) {
 	c.image.Set(x, y, color)
 }
 
-func NewCanvas() *CanvasPixelGl {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Term render",
-		Bounds: pixel.R(0, 0, 800, 800),
-		VSync:  true,
-	}
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	win.Clear(colornames.Skyblue)
-	img := image.NewRGBA(image.Rect(0, 0, 800, 800))
-	drawer := imdraw.New(pixel.MakePictureData(pixel.R(0, 0, 800, 800)))
-	return &CanvasPixelGl{image: img, drawer: drawer, Win: win}
-}
-
-func Render(c *CanvasPixelGl) {
-	c.Win.Clear(colornames.Black)
-	pic := pixel.PictureDataFromImage(c.image)
+func Render(window *pixelgl.Window, image *image.RGBA) {
+	window.Clear(colornames.Black)
+	pic := pixel.PictureDataFromImage(image)
 	sprite := pixel.NewSprite(pic, pic.Bounds())
-	sprite.Draw(c.Win, pixel.IM.Moved(c.Win.Bounds().Center()))
-	c.image = image.NewRGBA(image.Rect(0, 0, 800, 800))
-	c.Win.Update()
+	sprite.Draw(window, pixel.IM.Moved(window.Bounds().Center()))
+	window.Update()
 }
