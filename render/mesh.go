4af37dc0497e400473bb85a743f52d325bfd4f79
Bernardo de Oliveira Bruning <bernardo.bruning@resultadosdigitais.com.br>
2020-01-26 12:45:58 -0300

    create draw mesh

--- render/mesh.go
+++ render/mesh.go
@@ -1,5 +1,16 @@
 package render
 
+import (
+	"image/color"
+	"image/draw"
+)
+
 type Mesh struct {
 	triangles []Triangle
+}
+
+func (m Mesh) Draw(dst draw.Image) {
+	for _, triangle := range m.triangles {
+		triangle.Draw(dst, color.White)
+	}
 }
